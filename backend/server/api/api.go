397f0067db50b0c845a6c50e1cd9e695784e4c25
VarenytsiaMykhailo <52725148+VarenytsiaMykhailo@users.noreply.github.com>
2022-10-05 15:16:52 +0300

    development

--- backend/server/api/api.go
+++ backend/server/api/api.go
@@ -26,90 +26,117 @@ func GetApi() *Api {
 }
 
 func (api *Api) SignupUserHandler(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
+	if r.Method == "OPTIONS" {
+		w.Header().Set("Access-Control-Request-Headers", r.Header.Get("Access-Control-Request-Headers"))
+		w.Header().Set("Access-Control-Allow-Headers", r.Header.Get("Access-Control-Request-Headers"))
+		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:3004")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Request-Method", "POST")
+		w.WriteHeader(200)
+	} else {
+		defer r.Body.Close()
+		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:3004")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
-	parsedInput := new(models.User)
-	err := json.NewDecoder(r.Body).Decode(parsedInput)
-	if err != nil {
-		log.Println("Error while decode JSON:", err)
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-		return
+		parsedInput := new(models.User)
+		err := json.NewDecoder(r.Body).Decode(parsedInput)
+		if err != nil {
+			log.Println("Error while decode JSON:", err)
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+
+		log.Println(parsedInput)
+		err = api.usersStorage.AddUser(
+			parsedInput.NewUserData.Username,
+			parsedInput.NewUserData.Email,
+			parsedInput.NewUserData.Login,
+			parsedInput.NewUserData.Password,
+		)
+
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		api.usersStorage.PrintUsers()
+		w.WriteHeader(http.StatusOK)
 	}
-
-	log.Println(parsedInput)
-	err = api.usersStorage.AddUser(
-		parsedInput.NewUserData.Username,
-		parsedInput.NewUserData.Email,
-		parsedInput.NewUserData.Login,
-		parsedInput.NewUserData.Password,
-	)
-
-	if err != nil {
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
-
-	api.usersStorage.PrintUsers()
-
-	w.WriteHeader(http.StatusOK)
 }
 
 func (api *Api) CreateSessionHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
+	if r.Method == "OPTIONS" {
+		w.Header().Set("Access-Control-Request-Headers", r.Header.Get("Access-Control-Request-Headers"))
+		w.Header().Set("Access-Control-Allow-Headers", r.Header.Get("Access-Control-Request-Headers"))
+		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:3004")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Request-Method", "POST")
+		w.WriteHeader(http.StatusOK)
+	} else {
+		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:3004")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		parsedInput := new(models.Session)
+		err := json.NewDecoder(r.Body).Decode(parsedInput)
+		if err != nil {
+			log.Println("Error while decode JSON:", err)
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
 
-	parsedInput := new(models.Session)
-	err := json.NewDecoder(r.Body).Decode(parsedInput)
-	if err != nil {
-		log.Println("Error while decode JSON:", err)
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-		return
+		log.Println(parsedInput)
+
+		api.sessions = append(api.sessions, *parsedInput)
+
+		api.printSessions()
+
+		w.WriteHeader(http.StatusOK)
 	}
 
-	log.Println(parsedInput)
-
-	api.sessions = append(api.sessions, *parsedInput)
-
-	api.printSessions()
-
-	w.WriteHeader(http.StatusOK)
 }
 
 func (api *Api) FeedHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	articles, err := api.feedStorage.GetArticles()
-	if err != nil {
-		log.Println("Error while decode JSON:", err)
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
-
-	feed := models.Feed{}
-	for _, v := range articles {
-		article := models.Article{
-			Id:          v.Id,
-			Title:       v.Title,
-			Description: v.Description,
-			Tags:        v.Tags,
-			Category:    v.Category,
-			Rating:      v.Rating,
-			Authors:     v.Authors,
-			Content:     v.Content,
+	if r.Method == "OPTIONS" {
+		w.Header().Set("Access-Control-Request-Headers", r.Header.Get("Access-Control-Request-Headers"))
+		w.Header().Set("Access-Control-Allow-Headers", r.Header.Get("Access-Control-Request-Headers"))
+		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:3004")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Request-Method", "POST")
+		w.WriteHeader(200)
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:3004")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		articles, err := api.feedStorage.GetArticles()
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
 		}
-		feed.Articles = append(feed.Articles, article)
+
+		feed := models.Feed{}
+		for _, v := range articles {
+			article := models.Article{
+				Id:          v.Id,
+				Title:       v.Title,
+				Description: v.Description,
+				Tags:        v.Tags,
+				Category:    v.Category,
+				Rating:      v.Rating,
+				Authors:     v.Authors,
+				Content:     v.Content,
+			}
+			feed.Articles = append(feed.Articles, article)
+		}
+
+		log.Println(feed)
+
+		err = json.NewEncoder(w).Encode(&feed)
+		if err != nil {
+			log.Println("Error while encode JSON:", err)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
 	}
-
-	log.Println(feed)
-
-	err = json.NewEncoder(w).Encode(&feed)
-	if err != nil {
-		log.Println("Error while encode JSON:", err)
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
 }
 
 func (api *Api) printSessions() {
--- backend/server/server.go
+++ backend/server/server.go
@@ -11,10 +11,9 @@ func Run(serverAddress string) {
 	r := mux.NewRouter()
 
 	api := api.GetApi()
-
-	r.HandleFunc("/api/v1/user/signup", api.SignupUserHandler).Methods("POST")
-	r.HandleFunc("/api/v1/session/create", api.CreateSessionHandler).Methods("POST")
-	r.HandleFunc("/api/v1/feed", api.FeedHandler).Methods("GET")
+	r.HandleFunc("/api/v1/user/signup", api.SignupUserHandler).Methods("POST", "OPTIONS")
+	r.HandleFunc("/api/v1/session/create", api.CreateSessionHandler).Methods("POST", "OPTIONS")
+	r.HandleFunc("/api/v1/feed", api.FeedHandler).Methods("GET", "OPTIONS")
 
 	log.Println("Starting server on:", serverAddress)
 	http.ListenAndServe(serverAddress, r)
