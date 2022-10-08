6bb98af81804d02d9fd4b31d9dae45b56a0b42a8
aralary <makarov.0205@mail.ru>
2022-10-08 18:50:21 +0300

    + someth from another branch

--- server/server.go
+++ server/server.go
@@ -1,21 +1,31 @@
 package server
 
 import (
+	"2022_2_GoTo_team/server/api"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func routing(e *echo.Echo) {
-	api := api.GetApi()
-	e.GET("/", api.RootHandler)
-	e.POST("/login", api.LoginHandler)
-	e.POST("/logout", api.LogoutHandler)
+	ap := api.GetApi()
+	e.GET("/", ap.RootHandler)
+	e.POST("/login", ap.LoginHandler)
+	e.POST("/logout", ap.LogoutHandler)
+
+	e.POST("/api/v1/user/signup", ap.SignupUserHandler)
+	e.OPTIONS("/api/v1/user/signup", ap.SignupUserHandler)
+
+	e.POST("/api/v1/session/create", ap.CreateSessionHandler)
+	e.OPTIONS("/api/v1/session/create", ap.CreateSessionHandler)
+
+	e.GET("/api/v1/feed", ap.FeedHandler)
+	e.OPTIONS("/api/v1/feed", ap.FeedHandler)
 }
 
 func Run(servAddress string) {
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowMethods: []string{"POST", "GET"},
+		AllowMethods: []string{"POST", "GET", "OPTIONS"},
 	}))
 
 	routing(e)
