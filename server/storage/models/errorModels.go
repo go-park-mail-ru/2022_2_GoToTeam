dfa61f02ab75924c3ffe5e1e85bf0b270c66d4b4
aralary <makarov.0205@mail.ru>
2022-10-14 11:39:51 +0300

    + logout handler

--- server/storage/models/errorModels.go
+++ server/storage/models/errorModels.go
@@ -2,22 +2,32 @@ package models
 
 import "net/http"
 
-type ErrorResponse struct {
+type Response struct {
 	Status  int    `json:"status"`
 	Message string `json:"msg"`
 }
 
-var ErrUnpackingJSON = ErrorResponse{
+var ErrUnpackingJSON = Response{
 	Status:  http.StatusUnprocessableEntity,
 	Message: "Error unpacking JSON",
 }
 
-var ErrUserExist = ErrorResponse{
+var ErrUserExist = Response{
 	Status:  http.StatusFailedDependency,
 	Message: "User is already exist",
 }
 
-var ErrUserAuthorised = ErrorResponse{
+var ErrUserAuthorised = Response{
 	Status:  http.StatusFailedDependency,
 	Message: "Already authorised",
+}
+
+var ErrAlreadyLogout = Response{
+	Status:  http.StatusFailedDependency,
+	Message: "Already logout",
+}
+
+var LogoutResponse = Response{
+	Status:  http.StatusOK,
+	Message: "Goodbye, we are waiting for you again",
 }
