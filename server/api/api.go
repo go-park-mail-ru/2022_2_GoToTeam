f2ee20d1cde78617898832cd60014beb2d76bee3
aralary <makarov.0205@mail.ru>
2022-10-17 23:38:20 +0300

    + feed handler

--- server/api/api.go
+++ server/api/api.go
@@ -60,25 +60,6 @@ func (api *Api) IsAuthorized(c echo.Context) bool {
 	return authorized
 }
 
-//func (api *Api) RootHandler(w http.ResponseWriter, r *http.Request) {
-//	log.Println("Called RootHandler.")
-//	log.Println(api.serverAddress)
-//
-//	authorized := false
-//	session, err := r.Cookie("session_id")
-//	if err == nil && session != nil {
-//		_, authorized = api.sessions_[session.Value]
-//	}
-//
-//	if authorized {
-//		w.Write([]byte("Authrorized"))
-//		//w.WriteHeader(http.StatusOK)
-//	} else {
-//		//w.Write([]byte("not autrorized"))
-//		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
-//	}
-//}
-
 func (api *Api) RootHandler(c echo.Context) error {
 	return nil
 }
@@ -225,132 +206,9 @@ func (api *Api) SignupUserHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, response)
 }
 
-//func (api *Api) CreateSessionHandler(w http.ResponseWriter, r *http.Request) {
-//	log.Println("Called CreateSessionHandler.")
-//
-//	defer r.Body.Close()
-//	if r.Method == "OPTIONS" {
-//		w.Header().Set("Access-Control-Request-Headers", r.Header.Get("Access-Control-Request-Headers"))
-//		w.Header().Set("Access-Control-Allow-Headers", r.Header.Get("Access-Control-Request-Headers"))
-//		w.Header().Set("Access-Control-Allow-Origin", api.serverAddress)
-//		w.Header().Set("Access-Control-Allow-Credentials", "true")
-//		w.Header().Set("Access-Control-Request-Method", "POST")
-//		w.WriteHeader(http.StatusOK)
-//	} else {
-//		w.Header().Set("Access-Control-Allow-Origin", api.serverAddress)
-//		w.Header().Set("Access-Control-Allow-Credentials", "true")
-//		parsedInput := new(models.Session)
-//		err := json.NewDecoder(r.Body).Decode(parsedInput)
-//		if err != nil {
-//			log.Println("Error while decode JSON:", err)
-//			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-//			return
-//		}
-//
-//		log.Println(parsedInput)
-//
-//		//api.sessions = append(api.sessions, *parsedInput)
-//
-//		// ======================================
-//
-//		email := parsedInput.UserData.Email
-//		password := parsedInput.UserData.Password
-//		log.Println("URL", r.URL)
-//		log.Println("email", email)
-//		log.Println("password ", password)
-//
-//		//user, ok := api.users[r.FormValue("login")]
-//		user, err := api.usersStorage.GetUserByEmail(email)
-//		if err != nil {
-//			log.Println("Error ", err)
-//			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-//			return
-//		}
-//
-//		if user.Password != password {
-//			log.Println("Error ", err)
-//			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-//			return
-//		}
-//
-//		log.Println("____________________________________________________________________________________")
-//
-//		SID := RandStringRunes(32)
-//
-//		api.sessions_[SID] = user.UserId
-//
-//		cookie := &http.Cookie{
-//			Name:    "session_id",
-//			Path:    "/",
-//			Value:   SID,
-//			Expires: time.Now().Add(10 * time.Hour),
-//		}
-//		http.SetCookie(w, cookie)
-//
-//		api.printSessions()
-//
-//		w.Write([]byte(SID))
-//
-//		//w.WriteHeader(http.StatusOK)
-//	}
-//
-//}
-
 func (api *Api) CreateSessionHandler(c echo.Context) error {
 	return nil
 }
-
-//func (api *Api) FeedHandler(w http.ResponseWriter, r *http.Request) {
-//	log.Println("Called FeedHandler.")
-//
-//	if r.Method == "OPTIONS" {
-//		w.Header().Set("Access-Control-Request-Headers", r.Header.Get("Access-Control-Request-Headers"))
-//		w.Header().Set("Access-Control-Allow-Headers", r.Header.Get("Access-Control-Request-Headers"))
-//		w.Header().Set("Access-Control-Allow-Origin", api.serverAddress)
-//		w.Header().Set("Access-Control-Allow-Credentials", "true")
-//		w.Header().Set("Access-Control-Request-Method", "POST")
-//		w.WriteHeader(200)
-//	} else {
-//		//if api.IsAuthorized(w, r) {
-//		if true {
-//			w.Header().Set("Content-Type", "application/json")
-//			w.Header().Set("Access-Control-Allow-Origin", api.serverAddress)
-//			w.Header().Set("Access-Control-Allow-Credentials", "true")
-//			articles, err := api.feedStorage.GetArticles()
-//			if err != nil {
-//				log.Println("Error api.feedStorage.GetArticles", err)
-//				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-//				return
-//			}
-//
-//			feed := models.Feed{}
-//			for _, v := range articles {
-//				article := models.Article{
-//					Id:          v.Id,
-//					Title:       v.Title,
-//					Description: v.Description,
-//					Tags:        v.Tags,
-//					Category:    v.Category,
-//					Rating:      v.Rating,
-//					Authors:     v.Authors,
-//					Content:     v.Content,
-//				}
-//				feed.Articles = append(feed.Articles, article)
-//			}
-//
-//			log.Println(feed)
-//
-//			err = json.NewEncoder(w).Encode(&feed)
-//			if err != nil {
-//				log.Println("Error while encode JSON:", err)
-//				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-//				return
-//			}
-//		} else {
-//			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
-//		}
-//	}
-//}
 
 func (api *Api) FeedHandler(c echo.Context) error {
 	strId := c.QueryParam("idLastLoaded")
