a241f4feddbb565d36c67ffbed789d4cf2080c5d
VarenytsiaMykhailo <l2fonat@bk.ru>
2022-12-22 08:11:50 +0300

    Update and rename workflowDev.yml

--- pkg/utils/validators/validators_test.go
+++ pkg/utils/validators/validators_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestEmailIsValid(t *testing.T) {
 	res := EmailIsValidByRegExp("asd@asd.asd")
-	assert.NotEqual(t, true, res)
+	assert.Equal(t, true, res)
 }
 
 func TestLoginIsValidByRegExp(t *testing.T) {
