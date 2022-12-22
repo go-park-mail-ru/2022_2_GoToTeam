bb8daba7c45b4b907f99b72f53cd3a8c149bbc85
VarenytsiaMykhailo <l2fonat@bk.ru>
2022-12-22 04:45:19 +0300

    Change args params names

--- cmd/userProfileService/main.go
+++ cmd/userProfileService/main.go
@@ -10,7 +10,7 @@ const _DEFAULT_CONFIG_FILE_PATH = "configs/userProfileService/server.toml"
 var configFilePath string
 
 func init() {
-	flag.StringVar(&configFilePath, "config_file_path___", _DEFAULT_CONFIG_FILE_PATH, "config file path")
+	flag.StringVar(&configFilePath, "userProfileService_config_file_path", _DEFAULT_CONFIG_FILE_PATH, "config file path")
 }
 
 func main() {
