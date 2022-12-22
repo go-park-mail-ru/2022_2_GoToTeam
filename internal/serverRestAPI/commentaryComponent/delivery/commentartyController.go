5cc8de5fe7e09e9fa41003be350ce102d3d3b389
VarenytsiaMykhailo <l2fonat@bk.ru>
2022-12-22 14:12:02 +0300

    Refactoring, add easyjson

--- internal/serverRestAPI/commentaryComponent/delivery/commentartyController.go
+++ internal/serverRestAPI/commentaryComponent/delivery/commentartyController.go
@@ -33,24 +33,25 @@ func NewCommentaryController(commentaryUsecase commentaryComponentInterfaces.Com
 }
 
 func (cc *CommentaryController) CreateCommentaryHandler(c echo.Context) error {
-	cc.logger.LogrusLoggerWithContext(c.Request().Context()).Debug("Enter to the CreateCommentaryHandler function.")
+	ctx := c.Request().Context()
+	cc.logger.LogrusLoggerWithContext(ctx).Debug("Enter to the CreateCommentaryHandler function.")
 	defer c.Request().Body.Close()
 
 	parsedInputCommentary := new(createCommentary.Commentary)
 	if err := c.Bind(parsedInputCommentary); err != nil {
-		cc.logger.LogrusLoggerWithContext(c.Request().Context()).Warn(err)
+		cc.logger.LogrusLoggerWithContext(ctx).Warn(err)
 		return c.NoContent(http.StatusBadRequest)
 	}
 
-	cc.logger.LogrusLoggerWithContext(c.Request().Context()).Debugf("Parsed parsedInputCommentary: %#v", parsedInputCommentary)
+	cc.logger.LogrusLoggerWithContext(ctx).Debugf("Parsed parsedInputCommentary: %#v", parsedInputCommentary)
 
-	err := cc.commentaryUsecase.AddCommentaryBySession(c.Request().Context(), &models.Commentary{
+	err := cc.commentaryUsecase.AddCommentaryBySession(ctx, &models.Commentary{
 		Content:             parsedInputCommentary.Content,
 		ArticleId:           parsedInputCommentary.ArticleId,
 		CommentForCommentId: parsedInputCommentary.CommentForCommentId,
 	})
 	if err != nil {
-		cc.logger.LogrusLoggerWithContext(c.Request().Context()).Error(err)
+		cc.logger.LogrusLoggerWithContext(ctx).Error(err)
 		return c.NoContent(http.StatusInternalServerError)
 	}
 
@@ -58,30 +59,31 @@ func (cc *CommentaryController) CreateCommentaryHandler(c echo.Context) error {
 }
 
 func (cc *CommentaryController) GetAllCommentariesForArticle(c echo.Context) error {
-	cc.logger.LogrusLoggerWithContext(c.Request().Context()).Debug("Enter to the GetAllCommentariesForArticle function.")
+	ctx := c.Request().Context()
+	cc.logger.LogrusLoggerWithContext(ctx).Debug("Enter to the GetAllCommentariesForArticle function.")
 
 	parsedInputArticleIdStr := c.QueryParam("article")
-	cc.logger.LogrusLoggerWithContext(c.Request().Context()).Debugf("Parsed parsedInputArticleIdStr: %#v", parsedInputArticleIdStr)
+	cc.logger.LogrusLoggerWithContext(ctx).Debugf("Parsed parsedInputArticleIdStr: %#v", parsedInputArticleIdStr)
 	if parsedInputArticleIdStr == "" {
 		parsedInputArticleIdStr = "0"
 	}
 	parsedInputArticleId, err := strconv.Atoi(parsedInputArticleIdStr)
 	if err != nil {
-		cc.logger.LogrusLoggerWithContext(c.Request().Context()).Warn(err)
+		cc.logger.LogrusLoggerWithContext(ctx).Warn(err)
 		return c.NoContent(http.StatusBadRequest)
 	}
 	if parsedInputArticleId <= 0 {
-		cc.logger.LogrusLoggerWithContext(c.Request().Context()).Warn(errors.New(fmt.Sprintf("parsedInputArticleId = %d <= 0", parsedInputArticleId)))
+		cc.logger.LogrusLoggerWithContext(ctx).Warn(errors.New(fmt.Sprintf("parsedInputArticleId = %d <= 0", parsedInputArticleId)))
 		return c.NoContent(http.StatusBadRequest)
 	}
 
-	cc.logger.LogrusLoggerWithContext(c.Request().Context()).Debugf("Converted parsedInputArticleId: %#v", parsedInputArticleId)
+	cc.logger.LogrusLoggerWithContext(ctx).Debugf("Converted parsedInputArticleId: %#v", parsedInputArticleId)
 
-	commentaries, err := cc.commentaryUsecase.GetAllCommentariesForArticle(c.Request().Context(), parsedInputArticleId)
+	commentaries, err := cc.commentaryUsecase.GetAllCommentariesForArticle(ctx, parsedInputArticleId)
 	if err != nil {
 		switch errors.Unwrap(err).(type) {
 		default:
-			cc.logger.LogrusLoggerWithContext(c.Request().Context()).Error(err)
+			cc.logger.LogrusLoggerWithContext(ctx).Error(err)
 			return c.NoContent(http.StatusInternalServerError)
 		}
 	}
@@ -102,14 +104,19 @@ func (cc *CommentaryController) GetAllCommentariesForArticle(c echo.Context) err
 		}
 		allCommentariesForArticle.Commentaries = append(allCommentariesForArticle.Commentaries, commentary)
 	}
-	cc.logger.LogrusLoggerWithContext(c.Request().Context()).Debug("Formed allCommentariesForArticle: ", allCommentariesForArticle)
+	cc.logger.LogrusLoggerWithContext(ctx).Debug("Formed allCommentariesForArticle: ", allCommentariesForArticle)
 
-	return c.JSON(http.StatusOK, allCommentariesForArticle)
+	jsonBytes, err := allCommentariesForArticle.MarshalJSON()
+	if err != nil {
+		cc.logger.LogrusLoggerWithContext(ctx).Error(err)
+	}
+
+	return c.JSONBlob(http.StatusOK, jsonBytes)
 }
 
 func (cc *CommentaryController) LikeHandler(c echo.Context) error {
-	cc.logger.LogrusLoggerWithContext(c.Request().Context()).Debug("Enter to the LikeHandler function.")
 	ctx := c.Request().Context()
+	cc.logger.LogrusLoggerWithContext(ctx).Debug("Enter to the LikeHandler function.")
 	defer c.Request().Body.Close()
 
 	parsedInputLikeData := new(likeData.LikeData)
@@ -135,5 +142,10 @@ func (cc *CommentaryController) LikeHandler(c echo.Context) error {
 		Rating: updatedRating,
 	}
 
-	return c.JSON(http.StatusOK, likeResponse)
+	jsonBytes, err := likeResponse.MarshalJSON()
+	if err != nil {
+		cc.logger.LogrusLoggerWithContext(ctx).Error(err)
+	}
+
+	return c.JSONBlob(http.StatusOK, jsonBytes)
 }
--- internal/serverRestAPI/searchComponent/delivery/searchController.go
+++ internal/serverRestAPI/searchComponent/delivery/searchController.go
@@ -1,7 +1,6 @@
 package delivery
 
 import (
-	"2022_2_GoTo_team/internal/serverRestAPI/domain/customErrors/searchComponentErrors/usecaseToDeliveryErrors"
 	"2022_2_GoTo_team/internal/serverRestAPI/domain/interfaces/searchComponentInterfaces"
 	"2022_2_GoTo_team/internal/serverRestAPI/searchComponent/delivery/modelsRestApi"
 	"2022_2_GoTo_team/pkg/utils/logger"
@@ -28,8 +27,64 @@ func NewSearchController(searchUsecase searchComponentInterfaces.SearchUsecaseIn
 	return searchController
 }
 
+func (sc *SearchController) SearchHandler(c echo.Context) error {
+	ctx := c.Request().Context()
+	sc.logger.LogrusLoggerWithContext(ctx).Debug("Enter to the SearchHandler function.")
+
+	substringToSearch := c.QueryParam("substringToSearch")
+	sc.logger.LogrusLoggerWithContext(ctx).Debugf("Parsed substringToSearch: %#v", substringToSearch)
+	authorLogin := c.QueryParam("author")
+	sc.logger.LogrusLoggerWithContext(ctx).Debugf("Parsed authorLogin: %#v", authorLogin)
+	categoryName := c.QueryParam("category")
+	sc.logger.LogrusLoggerWithContext(ctx).Debugf("Parsed categoryName: %#v", categoryName)
+	tagName := c.QueryParam("tag")
+	sc.logger.LogrusLoggerWithContext(ctx).Debugf("Parsed tagName: %#v", tagName)
+
+	tags, err := sc.searchUsecase.GetArticlesBySearchParameters(ctx, substringToSearch, authorLogin, categoryName, tagName)
+	if err != nil {
+		switch errors.Unwrap(err).(type) {
+		default:
+			sc.logger.LogrusLoggerWithContext(ctx).Error(err)
+			return c.NoContent(http.StatusInternalServerError)
+		}
+	}
+
+	feed := modelsRestApi.Feed{}
+	for _, v := range tags {
+		article := modelsRestApi.Article{
+			Id:           v.ArticleId,
+			Title:        v.Title,
+			Description:  v.Description,
+			Tags:         v.Tags,
+			Category:     v.CategoryName,
+			Rating:       v.Rating,
+			Comments:     v.CommentsCount,
+			CoverImgPath: v.CoverImgPath,
+			Publisher: modelsRestApi.Publisher{
+				Username: v.Publisher.Username,
+				Login:    v.Publisher.Login,
+			},
+			CoAuthor: modelsRestApi.CoAuthor{
+				Username: v.CoAuthor.Username,
+				Login:    v.CoAuthor.Login,
+			},
+		}
+		feed.Articles = append(feed.Articles, article)
+	}
+	sc.logger.LogrusLoggerWithContext(c.Request().Context()).Debug("Formed feed: ", feed)
+
+	jsonBytes, err := feed.MarshalJSON()
+	if err != nil {
+		sc.logger.LogrusLoggerWithContext(ctx).Error(err)
+	}
+
+	return c.JSONBlob(http.StatusOK, jsonBytes)
+}
+
+/*
 func (sc *SearchController) SearchTagHandler(c echo.Context) error {
-	sc.logger.LogrusLoggerWithContext(c.Request().Context()).Debug("Enter to the SearchTagHandler function.")
+	ctx := c.Request().Context()
+	sc.logger.LogrusLoggerWithContext(ctx).Debug("Enter to the SearchTagHandler function.")
 
 	tag := c.QueryParam("tag")
 	sc.logger.LogrusLoggerWithContext(c.Request().Context()).Debugf("Parsed tag: %#v", tag)
@@ -37,14 +92,14 @@ func (sc *SearchController) SearchTagHandler(c echo.Context) error {
 		return c.NoContent(http.StatusBadRequest)
 	}
 
-	tags, err := sc.searchUsecase.GetArticlesByTag(c.Request().Context(), tag)
+	tags, err := sc.searchUsecase.GetArticlesByTag(ctx, tag)
 	if err != nil {
 		switch errors.Unwrap(err).(type) {
 		case *usecaseToDeliveryErrors.TagDoesntExistError:
-			sc.logger.LogrusLoggerWithContext(c.Request().Context()).Warn(err)
+			sc.logger.LogrusLoggerWithContext(ctx).Warn(err)
 			return c.NoContent(http.StatusNotFound)
 		default:
-			sc.logger.LogrusLoggerWithContext(c.Request().Context()).Error(err)
+			sc.logger.LogrusLoggerWithContext(ctx).Error(err)
 			return c.NoContent(http.StatusInternalServerError)
 		}
 	}
@@ -71,55 +126,13 @@ func (sc *SearchController) SearchTagHandler(c echo.Context) error {
 		}
 		feed.Articles = append(feed.Articles, article)
 	}
-	sc.logger.LogrusLoggerWithContext(c.Request().Context()).Debug("Formed feed: ", feed)
+	sc.logger.LogrusLoggerWithContext(ctx).Debug("Formed feed: ", feed)
 
-	return c.JSON(http.StatusOK, feed)
-}
-
-func (sc *SearchController) SearchHandler(c echo.Context) error {
-	sc.logger.LogrusLoggerWithContext(c.Request().Context()).Debug("Enter to the SearchHandler function.")
-
-	substringToSearch := c.QueryParam("substringToSearch")
-	sc.logger.LogrusLoggerWithContext(c.Request().Context()).Debugf("Parsed substringToSearch: %#v", substringToSearch)
-	authorLogin := c.QueryParam("author")
-	sc.logger.LogrusLoggerWithContext(c.Request().Context()).Debugf("Parsed authorLogin: %#v", authorLogin)
-	categoryName := c.QueryParam("category")
-	sc.logger.LogrusLoggerWithContext(c.Request().Context()).Debugf("Parsed categoryName: %#v", categoryName)
-	tagName := c.QueryParam("tag")
-	sc.logger.LogrusLoggerWithContext(c.Request().Context()).Debugf("Parsed tagName: %#v", tagName)
-
-	tags, err := sc.searchUsecase.GetArticlesBySearchParameters(c.Request().Context(), substringToSearch, authorLogin, categoryName, tagName)
+	jsonBytes, err := feed.MarshalJSON()
 	if err != nil {
-		switch errors.Unwrap(err).(type) {
-		default:
-			sc.logger.LogrusLoggerWithContext(c.Request().Context()).Error(err)
-			return c.NoContent(http.StatusInternalServerError)
-		}
+		sc.logger.LogrusLoggerWithContext(ctx).Error(err)
 	}
 
-	feed := modelsRestApi.Feed{}
-	for _, v := range tags {
-		article := modelsRestApi.Article{
-			Id:           v.ArticleId,
-			Title:        v.Title,
-			Description:  v.Description,
-			Tags:         v.Tags,
-			Category:     v.CategoryName,
-			Rating:       v.Rating,
-			Comments:     v.CommentsCount,
-			CoverImgPath: v.CoverImgPath,
-			Publisher: modelsRestApi.Publisher{
-				Username: v.Publisher.Username,
-				Login:    v.Publisher.Login,
-			},
-			CoAuthor: modelsRestApi.CoAuthor{
-				Username: v.CoAuthor.Username,
-				Login:    v.CoAuthor.Login,
-			},
-		}
-		feed.Articles = append(feed.Articles, article)
-	}
-	sc.logger.LogrusLoggerWithContext(c.Request().Context()).Debug("Formed feed: ", feed)
-
-	return c.JSON(http.StatusOK, feed)
+	return c.JSONBlob(http.StatusOK, jsonBytes)
 }
+*/
