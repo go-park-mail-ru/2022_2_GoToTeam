c40bd7d018425f64515bab6cacdc7bcbfa7b7ba5
FackOff25 <kozvladimir@bk.ru>
2022-12-30 20:29:25 +0300

    profile service privileges

--- database/installDB.sh
+++ database/installDB.sh
@@ -3,6 +3,7 @@ DB_NAME=ve_ru
 DB_USER=ve_ru_user
 DB_USER_PASS=ve_ru_password
 DB_AUTH_USER=ve_ru_auth_user
+DB_PROFILE_USER=ve_ru_profile_user
 su postgres <<EOF
 createdb  $DB_NAME;
 psql -c "CREATE USER $DB_USER WITH PASSWORD '$DB_USER_PASS';"
@@ -13,4 +14,7 @@ echo "Postgres User '$DB_USER' and database '$DB_NAME' created."
 psql -c "CREATE USER $DB_AUTH_USER WITH PASSWORD '$DB_USER_PASS';"
 psql -U postgres -d  $DB_NAME -c "GRANT SELECT ON users to $DB_AUTH_USER;"
 echo "Postgres User '$DB_AUTH_USER' created."
+psql -c "CREATE USER $DB_PROFILE_USER WITH PASSWORD '$DB_USER_PASS';"
+psql -U postgres -d  $DB_NAME -c "GRANT SELECT, UPDATE ON users to $DB_PROFILE_USER;"
+echo "Postgres User '$DB_PROFILE_USER' created."
 EOF
