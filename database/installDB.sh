8ef07b16d6249460bef3d988edd0314886f67e70
FackOff25 <kozvladimir@bk.ru>
2022-12-30 18:31:31 +0300

    configs generation

--- database/installDB.sh
+++ database/installDB.sh
@@ -2,6 +2,7 @@
 DB_NAME=ve_ru
 DB_USER=ve_ru_user
 DB_USER_PASS=ve_ru_password
+DB_AUTH_USER=ve_ru_auth_user
 su postgres <<EOF
 createdb  $DB_NAME;
 psql -c "CREATE USER $DB_USER WITH PASSWORD '$DB_USER_PASS';"
@@ -9,4 +10,7 @@ psql -U postgres -d $DB_NAME -f PostgreSQLdbGenScript.sql
 psql -U postgres -d  $DB_NAME -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public to $DB_USER;"
 psql -U postgres -d  $DB_NAME -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public to $DB_USER;"
 echo "Postgres User '$DB_USER' and database '$DB_NAME' created."
+psql -c "CREATE USER $DB_AUTH_USER WITH PASSWORD '$DB_USER_PASS';"
+psql -U postgres -d  $DB_NAME -c "GRANT SELECT ON users IN SCHEMA public to $DB_AUTH_USER;"
+echo "Postgres User '$DB_AUTH_USER' created."
 EOF
