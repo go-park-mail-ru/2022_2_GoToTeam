d2708e0f6aafd2fc5e2ffcdcf3eec02cb0ea155a
FackOff25 <54987169+FackOff25@users.noreply.github.com>
2023-01-01 15:03:03 +0300

    Vladimir bd (#32)
    * configs generation
    
    * configs generation
    
    * configs generation
    
    * configs generation
    
    * configs generation
    
    * configs generation
    
    * configs generation
    
    * configs generation
    
    * configs generation
    
    * configs generation
    
    * configs generation
    
    * privelages fix
    
    * profile service privileges
    
    * profile service privileges
    
    * main service privileges
    
    * main service privileges
    
    * users creation moved to sql scripts
    
    * users creation moved to sql scripts
    
    * postgresql cofig
    
    * postgresql config
    
    * temp cookie expire test comment
    
    * temp cookie expire test comment

--- database/dropDB.sh
+++ database/dropDB.sh
@@ -1,5 +1,11 @@
 #Drop ve_ru database
 DB_NAME=ve_ru
+DB_USER=ve_ru_user
+DB_AUTH_USER=ve_ru_auth_user
+DB_PROFILE_USER=ve_ru_profile_user
 su postgres <<EOF
 dropdb $DB_NAME
+psql -c "DROP ROLE $DB_USER;"
+psql -c "DROP ROLE $DB_AUTH_USER;"
+psql -c "DROP ROLE $DB_PROFILE_USER;"
 EOF
--- database/installDB.sh
+++ database/installDB.sh
@@ -1,12 +1,17 @@
-#installs DB. Requres installed and running postgresql
+#installs DB. Requires installed and running postgresql
 DB_NAME=ve_ru
 DB_USER=ve_ru_user
 DB_USER_PASS=ve_ru_password
+DB_AUTH_USER=ve_ru_auth_user
+DB_PROFILE_USER=ve_ru_profile_user
 su postgres <<EOF
 createdb  $DB_NAME;
-psql -c "CREATE USER $DB_USER WITH PASSWORD '$DB_USER_PASS';"
-psql -U postgres -d $DB_NAME -f PostgreSQLdbGenScript.sql
-psql -U postgres -d  $DB_NAME -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public to $DB_USER;"
-psql -U postgres -d  $DB_NAME -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public to $DB_USER;"
-echo "Postgres User '$DB_USER' and database '$DB_NAME' created."
+psql -U postgres -d $DB_NAME -f ./sql/PostgreSQLdbGenScript.sql
+echo "database '$DB_NAME' created."
+psql -U postgres -d  $DB_NAME -f ./sql/PostreSQLCreateMainUser.sql -v user_name='$DB_USER' -v user_password='$DB_USER_PASS'
+echo "Postgres User '$DB_USER' created."
+psql -U postgres -d  $DB_NAME -f ./sql/PostreSQLCreateAuthUser.sql -v user_name='$DB_AUTH_USER' -v user_password='$DB_USER_PASS'
+echo "Postgres User '$DB_AUTH_USER' created."
+psql -U postgres -d  $DB_NAME -f ./sql/PostreSQLCreateProfileUser.sql -v user_name='$DB_PROFILE_USER' -v user_password='$DB_USER_PASS'
+echo "Postgres User '$DB_PROFILE_USER' created."
 EOF
