2366bff7172bec67ea86afbd71841811e3e9de29
VarenytsiaMykhailo <l2fonat@bk.ru>
2023-01-03 06:17:01 +0300

    Update PostreSQLCreateMainUser.sql

--- database/sql/PostreSQLCreateMainUser.sql
+++ database/sql/PostreSQLCreateMainUser.sql
@@ -1,6 +1,6 @@
 CREATE USER :user_name WITH PASSWORD ':user_password';
 GRANT SELECT ON users, subscriptions, categories, articles, articles_likes, users_categories_subscriptions, comments, comments_likes, tags, tags_articles to :user_name;
 GRANT INSERT ON users, subscriptions, articles, articles_likes, users_categories_subscriptions, comments, comments_likes, tags_articles to :user_name;
-GRANT UPDATE ON subscriptions, articles, articles_likes, users_categories_subscriptions, comments, comments_likes, tags_articles to :user_name;
-GRANT DELETE ON subscriptions, articles, articles_likes, users_categories_subscriptions, comments, comments_likes, tags_articles to :user_name;
+GRANT UPDATE ON users, subscriptions, articles, articles_likes, users_categories_subscriptions, comments, comments_likes, tags_articles to :user_name;
+GRANT DELETE ON users, ubscriptions, articles, articles_likes, users_categories_subscriptions, comments, comments_likes, tags_articles to :user_name;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public to :user_name;
